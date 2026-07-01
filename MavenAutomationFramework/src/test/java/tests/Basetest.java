63c8cab698367a36f8da470919af83461af54b67
vishal5462 <singh2310vishal@gmail.com>
2026-07-01 17:14:45 +0530

    Add files via upload

---
+++ MavenAutomationFramework/src/test/java/tests/Basetest.java
@@ -0,0 +1,15 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Basetest {
+	
+	@Test
+	public void launchurl() {
+		
+		WebDriver driver = new ChromeDriver();
+	}
+
+}
